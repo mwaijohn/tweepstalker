5ede7c6dd536d0e13ce18451980cd16dce0c817e
mwai <mwaijohng@gmail.com>
2022-02-16 01:09:20 +0300

    design UI

--- src/Home.js
+++ src/Home.js
@@ -1,69 +1,83 @@
 import React from 'react'
 import Nav from './Nav'
+import StreakChart from './StreakChart'
 // import StreakBar from './StreakBar'
 
+
 function Home() {
+    const state = {
+        labels: ['January', 'February', 'March',
+            'April', 'May'],
+        datasets: [
+            {
+                label: 'Rainfall',
+                backgroundColor: 'rgba(75,192,192,1)',
+                borderColor: 'rgba(0,0,0,1)',
+                borderWidth: 2,
+                data: [65, 59, 80, 81, 56]
+            }
+        ]
+    }
     return (
         <div>
-            <Nav/>
+            <Nav />
             <br></br>
             <main>
-                <form>
-                    <input placeholder='Enter Username'></input>
-                    <button>Search</button>
+                <form className='w-full flex flex-wrap justify-center my-2'>
+                    <input placeholder='Enter Username' className='text-center w-96 border-2 rounded-full px-2 py-3 hover:border-red'></input>
+                    <button className='transition py-2 px-5 font-bold rounded-lg ml-1 bg-green-600 text-white sm:my-2 md:my-2'>Search</button>
                 </form>
-                <div>
-                    <div>
-                        <p>215</p>
-                        <p>Tweets</p>
+                <div className='flex my-3 justify-between m-auto p-3 w-9/12'>
+                    <div className='max-w-sm rounded overflow-hidden shadow-lg px-2 py-2 text-center w-72 h-24'>
+                        <p className='font-bold text-2xl'>100</p>
+                        <p>Composed Statuses</p>
                     </div>
-                    <div>
-                        <p>215</p>
+                    <div className='max-w-sm rounded overflow-hidden shadow-lg px-2 py-2 text-center w-72 h-24'>
+                        <p className='font-bold text-2xl'>1,245</p>
                         <p>Status Retweets</p>
                     </div>
-                    <div>
-                        <p>215</p>
-                        <p>Tweet Likes</p>
+                    <div className='max-w-sm rounded overflow-hidden shadow-lg px-2 py-2 text-center w-72 h-24'>
+                        <p className='font-bold text-2xl'>509</p>
+                        <p>Status Likes</p>
                     </div>
+
                 </div>
-                <div>
-                    <div>
-                        <p>215</p>
+                <div className='flex my-3 justify-between m-auto p-3 w-9/12'>
+                    <div className='max-w-sm rounded overflow-hidden shadow-lg px-2 py-2 text-center w-72 h-24'>
+                        <p className='font-bold text-2xl'>150</p>
                         <p>Replies Sent</p>
                     </div>
-                    <div>
-                        <p>215</p>
+                    <div className='max-w-sm rounded overflow-hidden shadow-lg px-2 py-2 text-center w-72 h-24'>
+                        <p className='font-bold text-2xl'>700</p>
                         <p>Replies Received</p>
                     </div>
-                    <div>
-                        <p>2.0</p>
+                    <div className='max-w-sm rounded overflow-hidden shadow-lg px-2 py-2 text-center w-72 h-24'>
+                        <p className='font-bold text-2xl'>2.0</p>
                         <p>Follow/Follower Ratio</p>
                     </div>
                 </div>
                 <div>
-                    {/* <StreakBar/> */}
-                    <div className="chat-notification">
-                        <div className="chat-notification-logo-wrapper">
-                            <img className="chat-notification-logo" src="/img/logo.svg" alt="ChitChat Logo"/>
-                        </div>
-                        <div className="chat-notification-content">
-                            <h4 className="chat-notification-title">ChitChat</h4>
-                            <p className="chat-notification-message">You have a new message!</p>
-                        </div>
-                    </div>
-                    <button className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">Message</button>
-
+                    <StreakChart/>
+                    
                 </div>
-
-                <div>
-                    <p>#exampletag</p>
-                    <p>#exampletag</p>
-                    <p>#exampletag</p>
-                    <p>#exampletag</p>
-                    <p>#exampletag</p>
-                    <p>#exampletag</p>
-                    <p>#exampletag</p>
-                    <p>#exampletag</p>
+                <div className='flex my-3 justify-between m-auto p-3 w-9/12 flex-col'>
+                    <h2 className='text-2xl'>Used hashtags</h2>
+                    <div className='flex flex-wrap'>
+                        <p className='p-2 my-1 mx-1 text-center rounded-full border-solid border-white bg-blue-600 text-white'>#exampletag</p>
+                        <p className='p-2 my-1 mx-1 text-center rounded-full border-solid border-white bg-blue-600 text-white'>#exa</p>
+                        <p className='p-2 my-1 mx-1 text-center rounded-full border-solid border-white bg-blue-600 text-white'>#exampletag</p>
+                        <p className='p-2 my-1 mx-1 text-center rounded-full border-solid border-white bg-blue-600 text-white'>#exampletag</p>
+                        <p className='p-2 my-1 mx-1 text-center rounded-full border-solid border-white bg-blue-600 text-white'>#exampletag</p>
+                        <p className='p-2 my-1 mx-1 text-center rounded-full border-solid border-white bg-blue-600 text-white'>#exampletag</p>
+                        <p className='p-2 my-1 mx-1 text-center rounded-full border-solid border-white bg-blue-600 text-white'>#exampletag</p>
+                        <p className='p-2 my-1 mx-1 text-center rounded-full border-solid border-white bg-blue-600 text-white'>#exampletag</p>
+                        <p className='p-2 my-1 mx-1 text-center rounded-full border-solid border-white bg-blue-600 text-white'>#exampletag</p>
+                        <p className='p-2 my-1 mx-1 text-center rounded-full border-solid border-white bg-blue-600 text-white'>#exampletag</p>
+                        <p className='p-2 my-1 mx-1 text-center rounded-full border-solid border-white bg-blue-600 text-white'>#exampletag</p>
+                        <p className='p-2 my-1 mx-1 text-center rounded-full border-solid border-white bg-blue-600 text-white'>#exampletag</p>
+                        <p className='p-2 my-1 mx-1 text-center rounded-full border-solid border-white bg-blue-600 text-white'>#exampletag</p>
+                        <p className='p-2 my-1 mx-1 text-center rounded-full border-solid border-white bg-blue-600 text-white'>#exampletag</p>
+                    </div>
                 </div>
             </main>
         </div>
--- src/Nav.js
+++ src/Nav.js
@@ -17,7 +17,7 @@ function Nav() {
                         <div>
                             <a href="#" class="flex items-center py-4 px-2">
                                 {/* <img src="logo.png" alt="Logo" class="h-8 w-8 mr-2"/> */}
-                                <span class="font-semibold text-gray-500 text-lg">Navigation</span>
+                                <span class="font-semibold text-gray-500 text-lg">Ndavigation</span>
                             </a>
                         </div>
                         <div class="hidden md:flex items-center space-x-1">
