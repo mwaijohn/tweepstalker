ae1c48f4eaefad2b4a6b64405a19d71510569a22
mwai <mwaijohng@gmail.com>
2022-02-19 15:53:53 +0300

    added utility to convert date to timestamp

--- src/utilities.js
+++ src/utilities.js
@@ -1,18 +1,19 @@
+// format date to YY-mm_DD
 export const formatDate = (date) => {
     let d = new Date(date),
         month = '' + (d.getMonth() + 1),
         day = '' + d.getDate(),
         year = d.getFullYear();
 
-    if (month.length < 2) 
+    if (month.length < 2)
         month = '0' + month;
-    if (day.length < 2) 
+    if (day.length < 2)
         day = '0' + day;
 
     return [year, month, day].join('-');
 }
 
-
+// get last seven days including today
 export const lastSevenDates = () => {
     let dates = []
     for (let i = 6; i >= 0; i--) {
@@ -21,4 +22,36 @@ export const lastSevenDates = () => {
         dates.push(newDate);
     }
     return dates;
+}
+
+// convert numbers like twitter
+export const numberFormatterLikeTwitter = (num, fixed) => {
+    // terminate early
+    if (num === null) {
+        return null;
+    }
+
+    // terminate early
+    if (num === 0) {
+        return '0';
+    }
+
+    fixed = !fixed || fixed < 0 ? 0 : fixed; // number of decimal places to show
+
+    let b = num.toPrecision(2).split('e'), // get power
+        k = b.length === 1 ? 0 : Math.floor(Math.min(b[1].slice(1), 14) / 3), // floor at decimals, ceiling at trillions
+        c =
+            k < 1
+                ? num.toFixed(0 + fixed)
+                : (num / Math.pow(10, k * 3)).toFixed(1 + fixed), // divide by power
+        d = c < 0 ? c : Math.abs(c), // enforce -0 is 0
+        e = d + ['', 'K', 'M', 'B', 'T'][k]; // append power
+
+    return e;
+}
+
+// convert date to timestamp
+export const dateToTimestamp = (strDate) => {
+    var datum = Date.parse(strDate);
+    return datum / 1000;
 }
