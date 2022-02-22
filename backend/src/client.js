86ea7c395cad672ca0fde074fa3c1415635ad42d
mwai <mwaijohng@gmail.com>
2022-02-22 16:49:34 +0300

    load 200 per call

--- backend/src/client.js
+++ backend/src/client.js
@@ -5,36 +5,28 @@ const getStatuses = async (screen_name, twitterClient) => {
     let payload = {}
 
     try {
-        for (var i = 6; i > 0; i--) {
+        for (var i = 3; i > 0; i--) {
             //get user time line
             if (max_id == null) {
-                payload = { "screen_name": screen_name, "include_rts": false }
+                payload = { "screen_name": screen_name, "include_rts": false, "count":200 }
             } else {
-                payload = { "screen_name": screen_name, "include_rts": false, "max_id": max_id }
+                payload = { "screen_name": screen_name, "include_rts": false, "max_id": max_id,"count":200 }
             }
             var statuses = await twitterClient.tweets.statusesUserTimeline(payload)
+          
             max_id = statuses[(statuses.length - 1)]['id']
+
 
             console.log(max_id)
             dataArray.unshift(...statuses)
         }
     } catch (error) {
         console.log("API Error: ",error)
+        return dataArray;
     }
 
-
-    // fs.writeFile('db.json', JSON.stringify(dataArray), function (err) {
-    //     if (err) return console.log(err);
-    //     console.log('Note added',dataArray.length);
-    // });
-    // console.log(JSON.stringify(dataArray))
     return dataArray
 }
 
-// var twitterClient = require('./twitterclient')
-
-// var client = twitterClient("jhjhj","jhjhjhj")
-
-// getStatuses("mjohn_5",client)
 
 module.exports = getStatuses
