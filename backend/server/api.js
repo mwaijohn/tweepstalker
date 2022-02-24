20b3446f23cd7066b41cad961744b4678c186ff1
mwai <mwaijohng@gmail.com>
2022-02-24 16:43:36 +0300

    cors

--- backend/server/api.js
+++ backend/server/api.js
@@ -4,9 +4,15 @@ const serverless = require("serverless-http");
 const dotenv = require('dotenv');
 dotenv.config();
 
+const cors = require('cors');
+
 const app = express();
+app.use(cors())
+
 var apiRouter = require('../routes/router.api');
+
 app.use(`/.netlify/functions/api`, apiRouter);
+
 
 module.exports = app;
 module.exports.handler = serverless(app);
