3026249f42fab3b515b43a53795fbbd9ce3aaebc
mwai <mwaijohng@gmail.com>
2022-03-02 15:22:51 +0300

    remove unused route

--- backend/routes/router.api.js
+++ backend/routes/router.api.js
@@ -15,13 +15,6 @@ apiRouter.route('/statuses/:username').get(authenticateToken, apiGetStatuses);
 apiRouter.route('/statuses').get(authenticateToken, apiGetStatuses);
 apiRouter.route('/twitter/users/profile_banner').get(authenticateToken, userProfile);
 
-apiRouter.route('/home').post((req,res)=>{
-    const CONSUMER_KEY = process.env.API_KEY;
-    const CONSUMER_SECRET = process.env.API_KEY_SECRET;
-    const CALLBACK_URL = process.env.CALLBACK_URL;
-    res.json({"KEY": CONSUMER_KEY,"SEC":`${CONSUMER_SECRET}`,"CALLB":`${CALLBACK_URL}`,"jhjhj":"hhghghfdfrg"})
-});
-
 module.exports = apiRouter;
 
 
