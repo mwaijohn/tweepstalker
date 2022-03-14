691da7c2e4af33e7f8c70c18eb73ddd15031f8e7
mwai <mwaijohng@gmail.com>
2022-03-14 22:05:12 +0300

    heroku port

--- backend/server/api.js
+++ backend/server/api.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require('body-parser')
 const dotenv = require('dotenv');
 dotenv.config();
-const port = 3000
+const port = process.env.PORT
 
 const cors = require('cors');
 
