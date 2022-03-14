e95f579f5a0846d93dfbfa05c0b6148b563d1548
mwai <mwaijohng@gmail.com>
2022-03-14 21:16:03 +0300

    change port

--- backend/server/api.js
+++ backend/server/api.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require('body-parser')
 const dotenv = require('dotenv');
 dotenv.config();
-const port = 8080
+const port = 3000
 
 const cors = require('cors');
 
